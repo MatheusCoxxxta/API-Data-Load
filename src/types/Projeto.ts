87afedd0c329b9b79e5b8ea5645dc95ebe4b4cd3
Matheus Costa <matheus.costa@zukk.com.br>
2021-03-14 13:57:00 -0300

    feat: creating type file to user

--- src/types/Projeto.ts
+++ src/types/Projeto.ts
@@ -2,4 +2,9 @@ export interface projeto {
   id: string;
   status: string;
   horas: any;
+  id_usuario: string;
+  dataInicio: string;
+  projetoNome: string;
+  concluido: boolean;
+  descricao: string;
 }
