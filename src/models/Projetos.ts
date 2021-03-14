0545fd42bf070e1dbce4b32b565b63d75c47c231
Matheus Costa <matheus.costa@zukk.com.br>
2021-03-14 13:56:06 -0300

    feat: adding new data structure

--- src/models/Projetos.ts
+++ src/models/Projetos.ts
@@ -1,6 +1,7 @@
 import { Model } from "objection";
 import connection from "../database/connection";
 import { projeto as ProjetosType } from "../types/Projeto";
+import Usuarios from "./Usuarios";
 
 Model.knex(connection);
 
@@ -19,9 +20,25 @@ class Projetos extends Model {
         id: { type: "string" },
         status: { type: "string", minLength: 1, maxLength: 255 },
         horas: { type: "float", minLength: 1, maxLength: 255 },
+        id_usuario: { type: "string", minLength: 1, maxLength: 255 },
+        dataInicio: { type: "string", minLength: 1, maxLength: 255 },
+        projetoNome: { type: "string", minLength: 1, maxLength: 255 },
+        concluido: { type: "boolean" },
+        descricao: { type: "string", minLength: 1, maxLength: 255 },
       },
     };
   }
+
+  static relationMappings = {
+    users: {
+      relation: Model.HasOneRelation,
+      modelClass: Usuarios,
+      join: {
+        from: "projetos.id_usuario",
+        to: "usuarios.id",
+      },
+    },
+  };
 }
 
 export default Projetos;
