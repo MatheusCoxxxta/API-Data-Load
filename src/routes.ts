ef9abf6993df1ac422559cb8added86311e8cd3d
Matheus Costa <matheus.costa@zukk.com.br>
2021-03-19 23:40:09 -0300

    feat: creating user list endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import DataController from "./controllers/DataController";
 import ProjectsController from "./controllers/ProjectsController";
+import UsersController from "./controllers/UsersController";
 
+const usersController = new UsersController();
 const projectsController = new ProjectsController();
 const dataController = new DataController();
 
@@ -15,5 +17,7 @@ routes.post("/joinData", dataController.joinData);
 routes.get("/listar/:page", dataController.listar);
 
 routes.get("/projects/:id", projectsController.show);
+
+routes.get("/users", usersController.index);
 
 export default routes;
