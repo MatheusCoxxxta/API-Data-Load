e2b4af19e46e65bfe75d342d5d7812848a4d1647
Matheus Costa <matheus.costa@zukk.com.br>
2021-05-02 21:29:39 -0300

    feat: creating update tasks function

--- src/routes.ts
+++ src/routes.ts
@@ -3,27 +3,45 @@ import DataController from "./controllers/DataController";
 import ProjectsController from "./controllers/ProjectsController";
 import UsersController from "./controllers/UsersController";
 import AuthController from "./controllers/AuthController";
+import TasksController from "./controllers/TasksController";
 import auth from "./middlewares/auth";
 
 const usersController = new UsersController();
 const projectsController = new ProjectsController();
 const dataController = new DataController();
 const authController = new AuthController();
+const tasksController = new TasksController();
 
 const routes = express.Router();
 
+/**
+ * ROOT
+ */
 routes.get("/", (request, response) => {
   response.send({ message: "Server online" });
 });
 
-routes.get("/listar", auth, dataController.listar);
-routes.get("/exportData", auth, dataController.exportData);
-
+/**
+ * PROJETO
+ */
 routes.get("/projects/:id", auth, projectsController.show);
 routes.get("/projects", auth, projectsController.list);
+routes.get("/exportData", auth, dataController.exportData);
 
+/**
+ * USER
+ */
 routes.get("/users", auth, usersController.index);
 
+/**
+ * AUTH
+ */
 routes.post("/login", authController.signIn);
+
+/**
+ * TAREFAS
+ */
+routes.get("/listar", auth, dataController.listar);
+routes.put("/task/:id", tasksController.update);
 
 export default routes;
