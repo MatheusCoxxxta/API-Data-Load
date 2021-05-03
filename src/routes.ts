b164add5543bc27e17abb3e0ff48be99bfd18590
Matheus Costa <matheus.costa@zukk.com.br>
2021-05-02 21:03:44 -0300

    feat: removing unused controllers

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,12 @@ import DataController from "./controllers/DataController";
 import ProjectsController from "./controllers/ProjectsController";
 import UsersController from "./controllers/UsersController";
 import AuthController from "./controllers/AuthController";
-import ProjectDataController from "./controllers/ProjectDataController";
-import TasksController from "./controllers/TasksController";
 import auth from "./middlewares/auth";
 
 const usersController = new UsersController();
 const projectsController = new ProjectsController();
 const dataController = new DataController();
 const authController = new AuthController();
-const projectDataController = new ProjectDataController();
-const tasksController = new TasksController();
 
 const routes = express.Router();
 
@@ -29,9 +25,5 @@ routes.get("/projects", auth, projectsController.list);
 routes.get("/users", auth, usersController.index);
 
 routes.post("/login", authController.signIn);
-
-routes.post("/seed/projects", projectDataController.storeProject);
-routes.post("/seed/users", projectDataController.storeUsers);
-routes.post("/seed/task", tasksController.storeTasks);
 
 export default routes;
