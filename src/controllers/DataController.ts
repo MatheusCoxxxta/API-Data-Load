22891a8059e6ac64ab56887524250e2f640e8ace
Matheus Costa <matheus.costa@zukk.com.br>
2021-05-05 13:24:49 -0300

    feat: getting project stats

--- src/controllers/DataController.ts
+++ src/controllers/DataController.ts
@@ -5,19 +5,35 @@ import fs from "fs";
 import Projetos from "../models/Projetos";
 import Usuarios from "../models/Usuarios";
 
-const getDataByUserId = async (projectId: any, userId: string) => {
-  const data = await knex("tarefas")
-    .join("usuarios", "usuarios.id", "tarefas.id_usuario")
-    .select(
-      "tarefas.id",
-      "tarefas.concluido",
-      "tarefas.status",
-      "tarefas.descricao",
-      "tarefas.horas"
-    )
+const getData = async (projectId: any, userId?: string) => {
+  let data;
 
-    .where("id_projeto", projectId)
-    .where("id_usuario", userId);
+  if (userId) {
+    data = await knex("tarefas")
+      .join("usuarios", "usuarios.id", "tarefas.id_usuario")
+      .select(
+        "tarefas.id",
+        "tarefas.concluido",
+        "tarefas.status",
+        "tarefas.descricao",
+        "tarefas.horas"
+      )
+
+      .where("id_projeto", projectId)
+      .where("id_usuario", userId);
+  } else {
+    data = await knex("tarefas")
+      .join("usuarios", "usuarios.id", "tarefas.id_usuario")
+      .select(
+        "tarefas.id",
+        "tarefas.concluido",
+        "tarefas.status",
+        "tarefas.descricao",
+        "tarefas.horas"
+      )
+
+      .where("id_projeto", projectId);
+  }
 
   const projeto = await Projetos.query()
     .select("*")
@@ -32,30 +48,73 @@ const getDataByUserId = async (projectId: any, userId: string) => {
   return tasks;
 };
 
-const getAllData = async (projectId: any) => {
-  const data = await knex("tarefas")
-    .join("usuarios", "usuarios.id", "tarefas.id_usuario")
-    .select(
-      "tarefas.id",
-      "tarefas.concluido",
-      "tarefas.status",
-      "tarefas.descricao",
-      "tarefas.horas"
-    )
-
-    .where("id_projeto", projectId);
-
-  const projeto = await Projetos.query()
-    .select("*")
-    .where("id", projectId)
-    .first();
-
-  const tasks = {
-    project: projeto.projetoNome,
-    tasks: data,
+const getStats = async (projectId: any, userId?: any) => {
+  let stats: {
+    IN_PROGRESS: number;
+    QA_TESTING: number;
+    QA_DEPLOYING: number;
+    RELEASE_TO_PROD: number;
+    FOR_TEST: number;
+    PROD_DEPLOYING: number;
+    DONE: number;
+  } = {
+    IN_PROGRESS: 0,
+    QA_TESTING: 0,
+    QA_DEPLOYING: 0,
+    RELEASE_TO_PROD: 0,
+    FOR_TEST: 0,
+    PROD_DEPLOYING: 0,
+    DONE: 0,
   };
 
-  return tasks;
+  let data;
+
+  if (userId) {
+    data = await knex("tarefas")
+      .join("usuarios", "usuarios.id", "tarefas.id_usuario")
+      .select(
+        "tarefas.id",
+        "tarefas.concluido",
+        "tarefas.status",
+        "tarefas.descricao",
+        "tarefas.horas"
+      )
+      .where("id_projeto", projectId)
+      .where("id_usuario", userId);
+  } else {
+    data = await knex("tarefas")
+      .join("usuarios", "usuarios.id", "tarefas.id_usuario")
+      .select(
+        "tarefas.id",
+        "tarefas.concluido",
+        "tarefas.status",
+        "tarefas.descricao",
+        "tarefas.horas"
+      )
+      .where("id_projeto", projectId);
+  }
+
+  stats.IN_PROGRESS = data.filter(
+    (element) => element.status === "IN_PROGRESS"
+  ).length;
+  stats.QA_TESTING = data.filter(
+    (element) => element.status === "QA_TESTING"
+  ).length;
+  stats.QA_DEPLOYING = data.filter(
+    (element) => element.status === "QA_DEPLOYING"
+  ).length;
+  stats.RELEASE_TO_PROD = data.filter(
+    (element) => element.status === "RELEASE_TO_PROD"
+  ).length;
+  stats.FOR_TEST = data.filter(
+    (element) => element.status === "FOR_TEST"
+  ).length;
+  stats.PROD_DEPLOYING = data.filter(
+    (element) => element.status === "PROD_DEPLOYING"
+  ).length;
+  stats.DONE = data.filter((element) => element.status === "DONE").length;
+
+  return stats;
 };
 
 class DataController {
@@ -66,9 +125,11 @@ class DataController {
 
     if (user.id_role === "0") {
       if (userId && projectId) {
-        const tasks = await getDataByUserId(projectId, userId);
+        const tasks = await getData(projectId, userId);
 
-        return response.status(200).json(tasks);
+        const stats = await getStats(projectId, userId);
+
+        return response.status(200).json({ stats, tasks });
       } else {
         return response.status(404).json({
           message: "Necessário informar projeto.",
@@ -76,8 +137,11 @@ class DataController {
       }
     } else {
       if (userId && projectId) {
-        const tasks = await getAllData(projectId);
-        return response.status(200).json(tasks);
+        const tasks = await getData(projectId);
+
+        const stats = await getStats(projectId);
+
+        return response.status(200).json({ stats, tasks });
       } else {
         return response.status(404).json({
           message: "Necessário informar projeto.",
